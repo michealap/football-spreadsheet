82a53bb994996b99ae34395cd9493fded3a85a40
Micheala Palmer <michealapalmer@gmail.com>
2022-03-01 22:39:15 -0500

    create static methods for MatchReader and Summary classes

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,10 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { ConsoleReport } from './reportTarget/ConsoleReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
-import {HtmlReport } from './reportTarget/HtmlReport';
 
-// create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
-// Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 matchReader.load();
 // matchReader.matches - array of matchData tuples
-
-const summary = new Summary(
-  // instance of analysis and console report
-  new WinsAnalysis('Man United'),
-  // new ConsoleReport()
-  new HtmlReport()
-)
-
 summary.buildAndPrintReport(matchReader.matches);
 
 
